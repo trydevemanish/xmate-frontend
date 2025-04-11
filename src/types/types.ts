129a6486945fd4a16a744704b691e39e128ef6dc
manish sharma dev <manishvsharma1@gmail.com>
2025-04-10 19:12:02 -1200

    created register page

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,9 @@ export type SigninFormValues = {
     email: string;
     password: string;
 };
+
+export type SignupFormValues = {
+    fullname:string;
+    email: string;
+    password: string;
+};
